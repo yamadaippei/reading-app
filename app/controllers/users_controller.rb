0b0dfd714b2bdddd8a8e1198dc17a081dd1cf458
yamadaippei <60645937+yamadaippei@users.noreply.github.com>
2020-04-09 18:25:43 +0900

    user login out
    # What
    userのログイン　ログアウトの設定
    
    # Why
    userのログイン　ログアウトを管理するため

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.ApplicationController
+    user.id = current_user.id
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image)
+  end
+end
